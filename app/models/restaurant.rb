a49b435364b355e771dee714e94e928e4f99a657
Cara Bell <carabell1994@aim.com>
2020-08-12 15:17:17 +0100

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
-  CATEGORY_LIST = ['chinese', 'italian', 'japanese', 'french', 'belgian'].freeze
+  CATEGORY_LIST = ['chinese', 'italian', 'japanese', 'french', 'belgian']
   validates :name, :address, :category, presence: true
   validates :category, inclusion: { in: CATEGORY_LIST }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = Restaurant.create([{ name: 'one', address: 'one', phone_number: 'one', category: 'belgian' },
+                                 { name: 'two', address: 'two', phone_number: 'two', category: 'french' },
+                                 { name: 'three', address: 'three', phone_number: 'three', category: 'japanese' },
+                                 { name: 'four', address: 'four', phone_number: 'four', category: 'italian' },
+                                 { name: 'five', address: 'five', phone_number: 'five', category: 'chinese' }])
